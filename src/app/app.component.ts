90ca72b6c6f4d327b4faebbbd21af19d3e5aab75
Hemant <hemant@Hemants-MacBook-Air.local>
2024-04-06 09:38:18 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,27 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ng17';
+
+  users = [
+    {
+      name: 'hemant',
+      age: 20,
+    },
+    {
+      name: 'hemant 1',
+      age: 78,
+    },
+    {
+      name: 'vinay',
+      age: 3,
+    },
+    {
+      name: 'mark',
+      age: 45,
+    },
+  ];
 }
